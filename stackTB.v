eb48f1b7afaf4d2ab06393116647f13525807eee
MobinaMhr <mobinamehrazar81@gmail.com>
2023-03-28 00:03:01 +0430

    Refactor some code!

--- stackTB.v
+++ stackTB.v
@@ -7,7 +7,7 @@ module stackTB();
         reg _push = 1'b0; 
         reg [3:0] _xIn, _yIn;
         wire [3:0] _xOut, _yOut;
-        // wire _fail = 1'b0;
+        wire _fail = 1'b0;
         stack CUT2(.clk(_clk), .xIn(_xIn), .yIn(_yIn), .rst(_rst), .push(_push), .pop(_pop), .xOut(_xOut), .yOut(_yOut), .fail(_fail));
 
         always #5 _clk <= ~_clk;
@@ -15,12 +15,15 @@ module stackTB();
                 #10;
                 _xIn = 4'h1;
                 _yIn = 4'h0;
+                #2;
                 _push = 1'b1;
                 #5;
                 _push = 1'b0;
-                #10;
+                #16;
                 _pop = 1'b1;
                 #5;
                 _pop = 1'b0;
+                #200 $stop;
         end
+
 endmodule
