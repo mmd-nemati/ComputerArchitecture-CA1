a6c6fab4c2d3ef8f864884af5edf4645bfbabba6
MobinaMhr <mobinamehrazar81@gmail.com>
2023-03-29 23:43:05 +0430

    Edit read map conditions and clean the code

--- mazeMemory.v
+++ mazeMemory.v
@@ -1,20 +1,15 @@
 `timescale 1ns/1ns
 
-module mazeMemory(clk, x, y, rd, wr, dIn, dOut);
+module mazeMemory(clk, x, y, en, rd, wr, dIn, dOut);
         input clk, dIn, rd, wr;
         input [3:0] x;
         input [3:0] y;
         output reg dOut;
         reg [0:15] map [0:15];
 
-	initial begin
-        $readmemh("map.txt", map); 
+        always @(posedge clk, posedge en) begin
+                $readmemh("map.txt", map); 
         end
-        // integer i = 0;
-        // initial begin
-        //         for (i = 0; i < 16; i = i + 1)
-        //                 $display("i : %d -> %b", i, map[i]);
-        // end
 
         always @(posedge clk, posedge rd) begin
                 dOut <= 1'b0;
