f2ab2f4dbfa7c9da49da8ab5093aee98b93b98e5
MobinaMhr <mobinamehrazar81@gmail.com>
2023-03-30 19:10:29 +0430

    Clean stack

--- stack.v
+++ stack.v
@@ -1,17 +1,19 @@
 `timescale 1ns/1ns
 
-module stack(clk, xIn, rst, yIn, push, pop, xOut, yOut, empty);
-        input clk, rst;
-        input [3:0] xIn, yIn;
-        input push, pop;
-        output reg [3:0] xOut, yOut;
-        output reg empty;
+module stack(clk, rst, locIn, push, pop, locOut, empStck);
+        input clk, rst, push, pop;
+        input [7:0] locIn;
+        output [7:0] locOut; 
+        output empStck;
+
+        assign empStck = ~(pointer == 6'b0);
+
         reg [7:0] stackMem [0:255];
         reg [5:0] pointer = 6'b0;
+        reg locOut;
         integer i = 0;
 
         always @(posedge clk, posedge rst) begin
-                {empty, xOut, yOut} = 9'b0;        
                 if (rst) begin
                         for (i = 0; i < 256; i = i + 1)
                                 stackMem[i] <= 8'h00;
@@ -20,18 +22,12 @@ module stack(clk, xIn, rst, yIn, push, pop, xOut, yOut, empty);
 
                 else if (push) begin
                         pointer = pointer + 1;
-                        stackMem[pointer][7:4] = xIn;
-                        stackMem[pointer][3:0] = yIn;
+                        stackMem[pointer] = locIn;
                 end
-                
+
                 else if (pop && pointer > 0) begin
-                        xOut = stackMem[pointer][7:4];
-                        yOut = stackMem[pointer][3:0];
-                        pointer = pointer - 1;
-                end
-                
-                else if (pop && pointer <= 0) begin
-                        empty = 1'b1;
+                        locOut = stackMem[pointer];
+                        pointer = pointer - 1;    
                 end
         end
 endmodule
