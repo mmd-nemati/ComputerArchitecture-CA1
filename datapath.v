58448c4ac05ba3124cf6835fddd6860797cbbb71
MobinaMhr <mobinamehrazar81@gmail.com>
2023-03-30 19:08:29 +0430

    Clean datapath

--- datapath.v
+++ datapath.v
@@ -1,22 +1,29 @@
 `timescale 1ns/1ns
 
-module datapath(clk, rst, ld, dir, xData, yData, wrong, sum);//new state.
-        input clk, rst, ld;
-        inout [3:0] xData, yData; //inOUT
+module datapath(clk, rst, rgLd, dir, currLoc, cntReach, nxtLoc);
+        input clk, rst, rgLd;
         input [1:0] dir;
-        output wrong;
-        output [3:0] sum;// updated location sum=x-if-sl==1 sum=y-if-sl==0
-        assign {xData, yData} = (sl) ? {sum, yData} : {xData, sum};
-        wire sl, co;
-        wire [3:0] b;
+        input [7:0] currLoc;
+        output [7:0] nxtLoc;
+        output cntReach;
 
-        wire [3:0] tmp = muxOut + dir[0];
-        assign wrong = (tmp == 4'b0);
+        wire [3:0] currX, currY;
+        wire sl;
+        wire co;
+        wire res;//updated location sum=x-if-sl==1 sum=y-if-sl==0
+        wire [3:0] toAdd;
+        wire [3:0] addTo;
+
+        wire [3:0] tmp = addTo + dir[0];
+
+        assign {currX, currY} = currLoc;
         assign sl = ^dir;// sl==0:y sl==1:x
-        wire [3:0] muxOut;
-        assign b = (dir[0] == 1'b0) ? -1 : 4'b1;
-        reg4b xLoc(.clk(clk), .rst(rst), .ld(ld), .data(xData));
-        reg4b yLoc(.clk(clk), .rst(rst), .ld(ld), .data(yData));
-        mux2To1 adderMux(.in0(yData), .in1(xData), .sl(sl), .out(muxOut)); 
-        adder add(.a(muxOut), .b(b), .ci(1'b0), .co(co), .sum(sum));
+        assign toAdd = dir[0]? 4'b1: -1;
+        assign nxtLoc = sl? {res, currY}: {currX, res};
+        assign cntReach = (tmp == 4'b0);
+
+        reg4b xLoc(.clk(clk), .rst(rst), .ld(rgLd), .data(currX));
+        reg4b yLoc(.clk(clk), .rst(rst), .ld(rgLd), .data(currY));
+        mux2To1 addrMux(.in0(currY), .in1(currX), .sl(sl), .ou(addTo));
+        adder add(.a(addTo), .b(toAdd), .ci(1'b0), .co(co), .sum(res));
 endmodule
