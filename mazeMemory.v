0a31f533971ce05d9d42139629027a201f64d450
MobinaMhr <mobinamehrazar81@gmail.com>
2023-03-30 19:09:41 +0430

    Clean mazeMemory

--- mazeMemory.v
+++ mazeMemory.v
@@ -1,14 +1,15 @@
 `timescale 1ns/1ns
 
-module mazeMemory(clk, x, y, en, rd, wr, dIn, dOut);
-        input clk, dIn, rd, wr;
-        input [3:0] x;
-        input [3:0] y;
-        output reg dOut;
-        reg [0:15] map [0:15];
+module mazeMemory(clk, loc, dIn, en, rd, wr, dOut);
+        input clk, dIn, en, rd, wr;
+        input [7:0] loc;
+        output dOut;
 
+        reg [0:15] map [0:15];
+        reg dOut;
+        
         always @(posedge clk, posedge en) begin
-                $readmemh("map.txt", map); 
+                $readmemh("map.txt", map);
         end
 
         always @(posedge clk, posedge rd) begin
@@ -17,9 +18,9 @@ module mazeMemory(clk, x, y, en, rd, wr, dIn, dOut);
                 if (rd) begin 
                         dOut <= map[x][y];
                 end
+
                 else if (wr) begin 
                         map[x][y] <= dIn;
-                end
+                end                
         end
-
 endmodule
