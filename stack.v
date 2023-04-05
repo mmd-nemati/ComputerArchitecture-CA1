cdd074a4097ee828a80f90c5575992a943c15e82
Mmd <mmd.nematiii@gmail.com>
2023-04-05 17:38:07 +0330

    Design new stack, not completed

--- stack.v
+++ stack.v
@@ -1,38 +1,50 @@
 `timescale 1ns/1ns
+`define STACK 6'b111111
+`define QUEUE 6'b000001
 
-module stack(clk, rst, locIn, push, pop, locOut, empStck);
-        input clk, rst, push, pop;
+module stack(clk, rst, locIn, push, pop, done, locOut, empStck); // Done should be only issued in one clock
+        input clk, rst, push, pop, done;
         input [7:0] locIn;
         output [7:0] locOut; 
         output empStck;
 
-        reg [5:0] pointer = 6'b1;
-        assign empStck = (pointer == 6'b0); // This should be under pointer declaration unless it gives some 
+        reg [5:0] headPointer = 6'b0;
+        reg [5:0] mainPointer = 6'b0;
+        reg pPopType = `STACK; 
+        assign empStck = (pPopType == `STACK)? (headPointer == 6'b0): (mainPointer == headPointer); // This should be under headPointer declaration unless it gives some 
                                              // weird compilation error about not declaring and double declaring :/
 
         reg [7:0] stackMem [0:255];
         reg [7:0] locOut;
         integer i = 0;
 
-        // always @(posedge clk) begin
-                // $display("S| toPop %d   %d:", stackMem[pointer-1][7:4], stackMem[pointer-1][3:0]);
-        // end
-
         always @(posedge clk, posedge rst) begin
-                if (rst) begin
-                        for (i = 0; i < 256; i = i + 1)
-                                stackMem[i] <= 8'h00;
-                        pointer = 6'b0;
+                if (done) begin 
+                        pPopType = 6'b000001;
+                        mainPointer = 6'b0;
+                        $display("Reached here, pPopType: %b %d", pPopType, pPopType);
                 end
 
-                else if (push) begin
-                        stackMem[pointer] = locIn;
-                        pointer = pointer + 1;
-                end
+                else begin 
+                        if (rst) begin
+                                for (i = 0; i < 256; i = i + 1)
+                                        stackMem[i] <= 8'h00;
+                                headPointer = 6'b0;
+                                mainPointer = 6'b0;
+                        end
 
-                else if (pop && pointer > 0) begin
-                        pointer = pointer - 1;
-                        locOut = stackMem[pointer];
+                        else if (push) begin
+                                headPointer = headPointer + 1;
+                                mainPointer = headPointer;
+                                stackMem[mainPointer] = locIn;
+                        end
+
+                        else if (pop && headPointer > 0) begin
+                                locOut = stackMem[mainPointer];
+                                headPointer = headPointer + pPopType;    
+                                mainPointer = headPointer;
+                        end
                 end
+                $display("done: %b, pPopType: %d, push: %b, pop: %b, headPointer: %d, mainPointer: %d, empStack: %b, stackX: %d, stackY: %d, stackMem[mainPointer]: %b",done, pPopType, push, pop, headPointer, mainPointer, empStck, locOut[7:4], locOut[3:0], stackMem[mainPointer]);
         end
 endmodule
--- stackTB.v
+++ stackTB.v
@@ -4,34 +4,50 @@ module stackTB();
         reg _clk = 1'b0;
         reg _rst = 1'b0;
         reg _pop = 1'b0;
-        reg _push = 1'b0; 
-        reg [3:0] _xIn, _yIn;
-        wire [3:0] _xOut, _yOut;
-        wire _fail = 1'b0;
+        reg _push = 1'b0;
+        reg _done = 1'b0; 
+        reg [7:0] _locIn;
+        wire [7:0] _locOut;
+        wire _empStck;
+// module stack(clk, rst, locIn, push, pop, done, locOut, empStck); // Done should be only issued in one clock
         
-        stack CUT2(.clk(_clk), .xIn(_xIn), .yIn(_yIn), .rst(_rst), .push(_push), .pop(_pop), .xOut(_xOut), .yOut(_yOut), .fail(_fail));
+        stack CUT2(.clk(_clk), .locIn(_locIn), .rst(_rst), .push(_push), .pop(_pop), .done(_done), .locOut(_locOut), .empStck(_empStck));
 
         always #5 _clk <= ~_clk;
         initial begin
                 #10;
-                _xIn = 4'h1;
-                _yIn = 4'h0;
+                _locIn = 8'h40;
                 #2;
                 _push = 1'b1;
                 #5;
                 _push = 1'b0;
                 #5;
-                _xIn = 4'h0;
-                _yIn = 4'h1;
+                _locIn = 8'h01;
                 #2;
                 _push = 1'b1;
                 #6;
                 _push = 1'b0;
                 _pop = 1'b1;
-                #20;
+                #6;
                 _pop = 1'b0;
-                #5;
+                _locIn = 8'h40;
+                #2;
+                _push = 1'b1;
+                #6;
+                _push = 1'b0;
+                #6;
+                _locIn = 8'ha9;
+                #2;
+                _push = 1'b1;
+                #6;
+                _push = 1'b0;
+                #25;
+                _done = 1'b1;
+                #6;
+                _done = 1'b0;
                 _pop = 1'b1;
+                
+
                 #200 $stop;
         end
 
